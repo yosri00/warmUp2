11dca69fb23e0d5dc5b30a92e691163f243f7ebb
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-02-29 09:21:46 +0100

    yosri

--- warmUp21/warmUp-21.js
+++ warmUp21/warmUp-21.js
@@ -12,7 +12,34 @@
 // Example 3:
 // Input: text = "RebootKamp"
 // Output: 0
- 
+
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+var maxNumberOfBalloons = function (text) {
+    let output = 0;
+    let map = {};
+
+    for (let i = 0; i < text.length; i++) {
+        if (!map[text[i]]) {
+            map[text[i]] = 1;
+        } else {
+            map[text[i]] = map[text[i]] + 1
+        }
+    }
+
+
+    while (true) {
+        if (map['b'] >= 1 && map['a'] >= 1 && map['l'] >= 2 && map['o'] >= 2 && map['n'] >= 1) {
+            map['b'] = map['b'] - 1;
+            map['a'] = map['a'] - 1;
+            map['l'] = map['l'] - 2;
+            map['o'] = map['o'] - 2;
+            map['n'] = map['n'] - 1;
+            output = output + 1
+
+        } else {
+            break;
+        }
+    } return output;
+};
